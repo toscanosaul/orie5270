9297cf96eaf8b5e6cd1738ea0879776fa99df5dd
Saul Toscano <toscano.saul@gmail.com>
2018-01-22 13:53:59 -0500

    sort numbers

---
+++ flask/sort_numbers/__init__.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/sorting', methods=['GET'])
+def sorting_get():
+    return render_template('sorting.html')
+
+@app.route('/sorting', methods=['POST'])
+def sorting_post():
+    els = request.form.get('values', None)
+
+    if els is None:
+        return 'No "values" parameter was provided', 400
+
+    try:
+        els = [int(e) for e in els.split(',')]
+    except ValueError as e:
+        return str(e), 400
+
+    return ','.join(map(str, sorted(els)))
